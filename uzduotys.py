b23e9e71a5e87bc40db61717ea8216481fe870f1
niekas <albertasgim@gmail.com>
2019-09-18 10:45:53 +0300

    Remove uniode characters

--- uzduotys.py
+++ uzduotys.py
@@ -2,7 +2,8 @@
 from random import seed, choice
 from sys import version, argv
 
-vardas_pavarde = ' '.join(argv[1:3])
+args = [a.strip('<').strip('>') for a in argv[1:3]]
+vardas_pavarde = ' '.join(args)
 if not version.startswith('3'):
     print('Naudokite python3')
     exit()
@@ -13,9 +14,9 @@ if not vardas_pavarde:
     exit()
 
 seed(vardas_pavarde)
-print(vardas_pavarde, 'užduotys:')
+print(vardas_pavarde, 'uzduotys:')
 print('Pirma:', choice(range(1, 21)))
 print('Antra:', choice(range(1, 30)))
-print('Trečia:', choice(range(1, 37)))
+print('Trecia:', choice(range(1, 37)))
 print('Ketvirta:', choice(range(1, 30)))
 print('Penkta:', choice(range(1, 38)))
