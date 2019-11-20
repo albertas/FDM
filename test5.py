c0284a7533972ebf69792c8a654fba776b9b92ae
niekas <albertasgim@gmail.com>
2019-11-20 09:27:36 +0200

    Fix 5'th 13

--- test5.py
+++ test5.py
@@ -97,7 +97,7 @@ culpa qui officia deserunt mollit anim id est laborum.
     ],
     13: [
         ['Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n',
-        'minim esse non'],
+        'aliqua minim esse non'],
     ],
     14: [
         ['Lorem ipsum dolllllllllllllor sit amet, connnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnsectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolllllllllore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ulllllllllllllllllllllamco laboris nisi ut aliquip ex ea commmmmmodo consequat. Duis aute irure dolllllllllllllor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Exxxxxxxxcepteur sint occaecat cuuuuupidatat non proident, sunt in culpa qui offffficia deserunt mollit anim id est laborum.\n',
