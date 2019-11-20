bf1c983908dc22cc673d55505ddc7d09b7514095
niekas <albertasgim@gmail.com>
2019-11-20 10:08:39 +0200

    Add ketvirta.py projection explanation

--- pavyzdziai/ketvirta.py
+++ pavyzdziai/ketvirta.py
@@ -2,7 +2,12 @@ import sys
 
 
 def projection(a, b, c):
-    '''https://stackoverflow.com/a/12499474'''
+    '''
+    a, b - taškai ant tiesės
+    c - taškas, kurį reikia suprojektuoti į a, b taškų tiesę
+    grąžinama c taško projekcija į a, b taškų tiesę
+    Remtasi: https://stackoverflow.com/a/12499474
+    '''
     x1 = a[0]
     y1 = a[1]
     x2 = b[0]
@@ -18,28 +23,35 @@ def projection(a, b, c):
     return [x, y]
 
 def test_projection():
+    # if projection([0,0],[1,1],[0,1]) != [0.5, 0.5]:
+    #     print('projection funkcija nekorektiškai veikia')
+    #     exit()
+
     assert projection([0,0],[1,1],[0,1]) == [0.5, 0.5]
     assert projection([0,0],[1,2],[0,1]) == [0.4, 0.8]
 
+    print('Visi testai įvykdyti sėkmingai!')
+
 
 if __name__ == '__main__':
-    if 'test' in sys.argv:
-        test_projection()
-    else:
-        zodynas = {}
 
-        for title in ['a', 'b', 'c']:
-            x = int(input('Įveskite '+title+' taško x koordinatę: '))
-            y = int(input('Įveskite '+title+' taško y koordinatę: '))
-            zodynas[title] = [x, y]
 
-        print(zodynas)      # {'a': [0, 0], 'b': [0, 1], 'c': [1, 1]}
 
-        zodynas['a'][0]  # 0
-        zodynas['a'][1]  # 0
 
-        bx, by = zodynas['b']
-        print(bx)
-        print(by)
+    zodynas = {}
 
-        print('Projekcijas:', projection(zodynas['a'], zodynas['b'], zodynas['c']))
+    for title in ['a', 'b', 'c']:
+        x = int(input('Įveskite '+title+' taško x koordinatę: '))
+        y = int(input('Įveskite '+title+' taško y koordinatę: '))
+        zodynas[title] = [x, y]
+
+    print(zodynas)      # {'a': [0, 0], 'b': [0, 1], 'c': [1, 1]}
+
+    zodynas['a'][0]  # 0
+    zodynas['a'][1]  # 0
+
+    bx, by = zodynas['b']
+    print(bx)
+    print(by)
+
+    print('Projekcijas:', projection(zodynas['a'], zodynas['b'], zodynas['c']))
