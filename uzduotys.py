f662d189d9c48d7c698f757dc2aa58c4dbdbef02
niekas <albertasgim@gmail.com>
2020-09-16 10:03:04 +0300

    Use lower case ascii name
    as a seed when selecting task numbers.

--- uzduotys.py
+++ uzduotys.py
@@ -1,20 +1,32 @@
 #! coding: utf-8
 from random import seed, choice
 from sys import version, argv
+import unicodedata
 
-args = [a.strip('<').strip('>') for a in argv[1:3]]
-vardas_pavarde = ' '.join(args)
+args = [a.strip('<').strip('>') for a in argv[1:]]
 if not version.startswith('3'):
     print('Naudokite python3')
     exit()
 
-if not vardas_pavarde:
-    print('Naudojimo pavyzdys:')
-    print('$ python3 uzduotys.py Albertas Gimbutas')
+if len(args) < 2:
+    try:
+        print('Naudojimo pavyzdys (nurodykite savo vardą ir pavardę vietoje Albertas Gimbutas):')
+        print('$ python3 uzduotys.py Albertas Gimbutas')
+    except:
+        print('Naudojimo pavyzdys (nurodykite savo varda ir pavarde vietoje Albertas Gimbutas):')
+        print('$ python3 uzduotys.py Albertas Gimbutas')
     exit()
 
+
+vardas_pavarde = ' '.join(args).lower()
+vardas_pavarde = unicodedata.normalize('NFKD', vardas_pavarde).encode('ascii','ignore').decode()
+
 seed(vardas_pavarde)
-print(vardas_pavarde, 'uzduotys:')
+try:
+    print(f'"{" ".join(args)}" užduočių numeriai:')
+except:
+    print(f'"{" ".join(args)}" uzduociu numeriai:')
+
 print('Pirma:', choice(range(1, 21)))
 print('Antra:', choice(range(1, 30)))
 print('Trecia:', choice(range(1, 37)))
